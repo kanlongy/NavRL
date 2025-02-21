458cf44678ccb9d0dbe68594437e7d8dd0dbc5fa
Zhefan-Xu <zhefanx@andrew.cmu.edu>
2025-02-21 10:24:38 -0500

    update

--- isaac-training/setup.sh
+++ isaac-training/setup.sh
@@ -29,7 +29,8 @@ echo "Running orbit.sh setup..."
 conda activate $ENV_NAME
 pip install numpy==1.26.4
 pip install "pydantic!=1.7,!=1.7.1,!=1.7.2,!=1.7.3,!=1.8,!=1.8.1,<2.0.0,>=1.6.2"
-
+pip install imageio-ffmpeg==0.4.9
+pip install moviepy==1.0.3
 
 # Step 2: Install dependencies
 echo "Installing system dependencies..."
